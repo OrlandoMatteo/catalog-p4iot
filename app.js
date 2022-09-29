8fb0e2f2e397d71ba2ca30ac1955415108f6a2e8
Matteo Orlando <matteo.orlando.1993@gmail.com>
2022-09-29 13:03:33 +0200

    edit endpoint and index

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3001;
-const catalog=require("./catalog.json")
-app.get("/", (req, res) =>{
+const catalog = require("./catalog.json")
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+app.get("/all", (req, res) => {
     res.send(catalog)
 });
-app.get("/devices", (req, res) =>{
+app.get("/devices", (req, res) => {
     res.send(catalog.devicesList)
 });
-app.get("/device/:deviceID", (req, res) =>{
-    const toFind=req.params.deviceID
-    const device= catalog.devicesList.find(({deviceID})=>deviceID==toFind)
+app.get("/device/:deviceID", (req, res) => {
+    const toFind = req.params.deviceID
+    const device = catalog.devicesList.find(({ deviceID }) => deviceID == toFind)
     res.send(device)
 });
-app.get("/houses", (req, res) =>{
+app.get("/houses", (req, res) => {
     res.send(catalog.housesList)
 });
-app.get("/house/:houseID", (req, res) =>{
-    const toFind=req.params.houseID
-    const house= catalog.housesList.find(({houseID})=>houseID==toFind)
+app.get("/house/:houseID", (req, res) => {
+    const toFind = req.params.houseID
+    const house = catalog.housesList.find(({ houseID }) => houseID == toFind)
     res.send(house)
 });
-app.get("/users", (req, res) =>{
+app.get("/users", (req, res) => {
     res.send(catalog.usersList)
 });
-app.get("/user/:userID", (req, res) =>{
-    const toFind=req.params.userID
-    const user= catalog.usersList.find(({userID})=>userID==toFind)
+app.get("/user/:userID", (req, res) => {
+    const toFind = req.params.userID
+    const user = catalog.usersList.find(({ userID }) => userID == toFind)
     res.send(user)
 });
-app.get("/mqtt", (req, res) =>{
+app.get("/mqtt", (req, res) => {
     res.send(catalog.broker)
 });
 
