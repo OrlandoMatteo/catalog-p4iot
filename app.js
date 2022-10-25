11927460c5b4771ae1e6947b95f45032fd0219d7
Matteo Orlando <matteo.orlando.1993@gmail.com>
2022-10-25 10:00:06 +0200

    prettier

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 const catalog = require("./catalog.json")
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
